ffd00ebf7f3206ab0ca62711816fa885e5843e4b
Aggelos Karapatakis <63917743+Karapatakis106@users.noreply.github.com>
2021-01-27 22:17:47 +0200

    Create arch.js

---
+++ arch.js
@@ -0,0 +1,6 @@
+if (navigator.userAgent.indexOf("WOW32") != -1 || 
+        navigator.userAgent.indexOf("Win32") != -1 ){
+       document.write("Your architecture is 32 bit");
+    } else {
+       document.write("Your architecture is 64 bit");
+    }
