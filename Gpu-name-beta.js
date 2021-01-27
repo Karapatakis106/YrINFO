40c97d1e3c0b64f802d4851fbe70dbf31d972432
Aggelos Karapatakis <63917743+Karapatakis106@users.noreply.github.com>
2021-01-28 00:10:58 +0200

    Create Gpu-name-beta.js

---
+++ Gpu-name-beta.js
@@ -0,0 +1,38 @@
+function getVideoCardInfo() {
+// Finding Graphics Card name, using openGL. Might be unstable
+
+  const gl = document.createElement('canvas').getContext('webgl');
+
+  if (!gl) {
+
+    return {
+
+      error: "no webgl",
+
+    };
+
+  }
+
+  const debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+
+  return debugInfo ? {
+
+    vendor: gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL),
+
+    renderer:  gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL),
+
+  } : {
+
+    error: "no WEBGL_debug_renderer_info",
+
+  };
+
+}
+
+console.log(getVideoCardInfo());
+
+// on index.html
+
+document.write("Graphics Card Name: )
+document.write()
+document.write(getVideoCardInfo())
